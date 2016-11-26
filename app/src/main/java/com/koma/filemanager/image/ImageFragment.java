5d3f436e8802de44e2a93bc61f07ab5e0a42e756
koma <koma_mj@icloud.com>
2016-11-26 17:52:49 +0800

    Modify data layer.

--- app/src/main/java/com/koma/filemanager/image/ImageFragment.java
+++ app/src/main/java/com/koma/filemanager/image/ImageFragment.java
@@ -1,8 +1,26 @@
 package com.koma.filemanager.image;
 
+import com.koma.filemanager.base.BaseFragment;
+import com.koma.filemanager.util.LogUtils;
+
 /**
  * Created by koma on 2016/11/26.
  */
 
-public class ImageFragment {
+public class ImageFragment extends BaseFragment implements ImageConstract.View {
+    private static final String TAG = "ImageFragment";
+    @Override
+    public void showEmptyView() {
+        LogUtils.i(TAG,"showEmptyView");
+    }
+
+    @Override
+    public void refreshAdapter() {
+        LogUtils.i(TAG,"refreshAdapter");
+    }
+
+    @Override
+    public void setPresenter(ImageConstract.Presenter presenter) {
+        LogUtils.i(TAG,"setPresenter");
+    }
 }
--- app/src/main/java/com/koma/filemanager/image/ImagePresenter.java
+++ app/src/main/java/com/koma/filemanager/image/ImagePresenter.java
@@ -1,8 +1,31 @@
 package com.koma.filemanager.image;
 
+import com.koma.filemanager.data.model.ImageFile;
+import com.koma.filemanager.util.LogUtils;
+
+import java.util.List;
+
+import rx.Observable;
+
 /**
  * Created by koma on 2016/11/26.
  */
 
-public class ImagePresenter {
+public class ImagePresenter implements ImageConstract.Presenter {
+    private static final String TAG = "ImagePresenter";
+    @Override
+    public Observable<List<ImageFile>> getImageFiles() {
+        LogUtils.i(TAG,"getImageFiles");
+        return null;
+    }
+
+    @Override
+    public void subscribe() {
+        LogUtils.i(TAG,"subscribe");
+    }
+
+    @Override
+    public void unSubscribe() {
+        LogUtils.i(TAG,"unSubscribe");
+    }
 }
